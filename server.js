da22244a2bf59446aea15575389e906dbc2aebab
Soxe253 <soxe@pugetsound.edu>
2022-11-04 12:40:07 -0700

    updated server.js to the new example

--- server.js
+++ server.js
@@ -3,27 +3,46 @@
 */
 
 const express = require("express");
-
+const bodyParser = require("body-parser"); // If we get data in a POST, this will parse it for us
 
 // Creates an Express application: https://expressjs.com/en/4x/api.html#app
 // Returns the Express application object
 const app = express();
 const port = 3000;
 
-// Tell express where to find your CSS, JS, and images
+// Tell express object where to find your CSS, JS, and images
 app.use(express.static('public'));
 app.use('/css', express.static(__dirname + 'public/css'));
 app.use('/js', express.static(__dirname + 'public/js'));
 app.use('/img', express.static(__dirname + 'public/img'));
 
+// Register middleware to be used
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
 
-// A route definition
+
+// Route definitions
 app.get("/", (req, res) => {
     res.sendFile(__dirname + '/views/index.html');
 });
 
 
+// Prints the request body so you can see the json data sent in the request
+// Sends back a response with the phrase "You did it!" for congratulations
+app.post("/postButton", (req, res) => {
+    console.log(req.body);
+    res.json({ text: "You did it!" });
+})
 
+app.get("/dogs", (req, res) => {
+    console.log('got here');
+    res.sendFile(__dirname + '/views/dogs.html');
+});
+
+app.post("/login", (req, res) => {
+    console.log(req.body);
+    res.send('<h1>Successful Login</h1>');
+});
 
 // starts web server listening on localhost at port 3000
 app.listen(port, () => {
