3144f6ae9ef3d62d706d6a60e4632ecdcf2beeb1
superawesomepi <jordan3vh@gmail.com>
2022-12-12 19:28:58 -0800

    Added user information to profile page

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,1 +1,4 @@
-console.log(localStorage.getItem('Username'));
+let username = localStorage.getItem('Username');
+console.log(username);
+const text = document.createTextNode(username.substring(1, username.length - 1));
+document.getElementById("username").appendChild(text);
