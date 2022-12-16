0686408f43c5a1faf8292f6b07c6e7ba0b379abc
Soxe253 <soxe@pugetsound.edu>
2022-12-15 16:43:34 -0800

    Fixed problem where site would crash on wronglogin

--- public/js/home.js
+++ public/js/home.js
@@ -27,7 +27,8 @@ async function load (){
         method: 'GET',
         headers: {
             'Content-Type' : 'application/json'
-        }
+        },
+        body: localStorage.getItem('GroupCode')
     });
     let body = await response.text();
     let posts = body.split("\n");
